474cd6742eaa526a4925ba764344522a54939e27
Tomi Nevalainen <Tomi.Nevalainen@edu.karelia.fi>
2013-10-14 15:05:33 +0300

    MainScene and rotation script

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class RotateObject : MonoBehaviour {
+	
+	float speed = 20.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,speed * Time.deltaTime,0);
+	}
+}
